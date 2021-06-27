e817b2b80fe8dc9e89a9c07e3bb4b297dd72844d
rchettiar7 <60092801+rchettiar7@users.noreply.github.com>
2021-06-27 17:13:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Note";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+export default App;
